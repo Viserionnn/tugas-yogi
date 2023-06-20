7ace703214e9c98fe0057c1b7e4b3ac9482911cc
Viserion <121105647+Viserionnn@users.noreply.github.com>
2023-06-20 17:20:01 +0700

    Add files via upload

---
+++ yogi/yogi/home.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("Tugas Elektronika Analog")
+st.header("Transistor Sebagai Penguat")
+st.write("  ")
+st.image("download.jpg")
+st.write("   ")
+st.header("Yogi Firmasnyah || 11-2021-030 || Teknik Elektro")
+st.header("Dosen Pengajar : Ir.Rustamaji,M.T.")
